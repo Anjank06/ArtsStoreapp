d0961b3a9781c7a49bba7d05885e3287ac71f0be
Anjan Kumar <anjanbhat1997@gmail.com>
2024-05-19 12:31:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. by anjan kumar
+          Edit src/App.js and save to reload. by anjan kumar
         </p>
         <a
           className="App-link"
