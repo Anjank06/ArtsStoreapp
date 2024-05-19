63cb0f7dd69041796ef571c1f82abcb93b2b9d30
Anjan Kumar <anjanbhat1997@gmail.com>
2024-05-19 18:36:39 +0530

    deploying components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './utils/logo.svg';
+// import logo from './utils/logo.svg';
 import './App.css';
 
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-// import Companies from './components/Companies/Conpanies';
-// import Residencies from "./components/Residencies/Residencies";
-// import Contact from './components/Contact/Contact';
-// import GetStarted from './components/GetStarted/GetStarted';
-// import Footer from './components/Footer/Footer';
+import Companies from './components/Companies/Conpanies';
+import Residencies from "./components/Residencies/Residencies";
+import Contact from './components/Contact/Contact';
+import GetStarted from './components/GetStarted/GetStarted';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <div>
         <div className="white-gradient" />
         <Header />
         <Hero />
       </div>
-      </header>
+      <Companies />
+      <Residencies />
+      <Contact/>
+      <GetStarted/>
+      <Footer/>
     </div>
   );
 }
