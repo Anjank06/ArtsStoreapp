4ba199c8bfaa8aaceca369eb39f5b629c07ce97c
Anjan Kumar <anjanbhat1997@gmail.com>
2024-05-19 18:15:05 +0530

    pics update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import logo from './utils/logo.svg';
 import './App.css';
 
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+// import Companies from './components/Companies/Conpanies';
+// import Residencies from "./components/Residencies/Residencies";
+// import Contact from './components/Contact/Contact';
+// import GetStarted from './components/GetStarted/GetStarted';
+// import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <div>
         <div className="white-gradient" />
         <Header />
+        <Hero />
       </div>
       </header>
     </div>
